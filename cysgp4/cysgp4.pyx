0be60b551b90b33aeca9d04abb74f8522d92c482
Benjamin Winkel <bwinkel@mpifr.de>
2019-11-15 16:43:09 +0100

    start to add docstrings

--- cysgp4/cysgp4.pyx
+++ cysgp4/cysgp4.pyx
@@ -103,7 +103,86 @@ cdef inline double mjd_from_ticks(long long ticks) nogil:
 
 cdef class PyDateTime(object):
     '''
-    Wrapper around (C++) DateTime class
+    Thin wrapper around sgp4 (C++) DateTime class.
+
+    Internally, DateTime objects store time on a linear scale, "ticks",
+    which is the number of micro-seconds since 1. January 0001, 00:00.
+    For convenience, one can convert to and from Python `datetime
+    <https://docs.python.org/3.8/library/datetime.html>`_ objects and
+    `Modified Julian Date (MJD)
+    <https://en.wikipedia.org/wiki/Julian_day#Variants>`_, which is often
+    used in astronomy.
+
+    For calculating the position of a satellite for a given observer
+    (i.e., in the horizontal frame, Azimuth and Elevation), one needs
+    the `local mean sidereal time
+    <https://en.wikipedia.org/wiki/Sidereal_time>`_  (derived from Greenwhich
+    mean sidereal time), which can be queried using the
+    `~cysgp4.PyDateTime.lmst()` and `~cysgp4.PyDateTime.gmst()` methods.
+
+    Parameters
+    ----------
+    dt : `~datetime.datetime` object, or None (default: None)
+        Construct the `~cysgp4.PyDateTime` instance with a given
+        Python `~datetime.datetime` object, or - if None - use the
+        current date and time (as given by `~datetime.datetime.now()`)
+    init : Boolean (default: True)
+        If set to true, the `~datetime.datetime` object will ignore the
+        `dt` parameter and set the internal ticks variable to 0.
+        This is only useful for computationally heavy tasks.
+
+    Returns
+    -------
+    pydt : `~cysgp4.PyDateTime` object
+
+    Examples
+    --------
+    There are several possibilities to create a `~cysgp4.PyDateTime` object::
+
+        >>> from datetime import datetime
+        >>> from cysgp4 import PyDateTime
+
+        >>> # from Python's datetime
+        >>> dt = datetime(2019, 1, 1, 12, 13, 14)
+        >>> pydt = PyDateTime(dt)
+        >>> pydt
+        <PyDateTime: 2019-01-01 12:13:14.000000 UTC>
+
+        >>> # from Modified Julian Date
+        >>> mjd = 56458.123
+        >>> pydt = PyDateTime.from_mjd(mjd)
+        >>> pydt
+        <PyDateTime: 2013-06-15 02:57:07.199999 UTC>
+
+        >>> # from "Ticks"
+        >>> ticks = 58628880000000000  # aka MJD zero point
+        >>> pydt = PyDateTime.from_ticks(ticks)
+        >>> pydt
+        <PyDateTime: 1858-11-17 00:00:00.000000 UTC>
+
+    Accessing and Modifying the date and time is done via the following
+    properties and methods::
+
+        >>> print(pydt.datetime)
+        1858-11-17 00:00:00
+        >>> pydt.datetime = datetime(2010, 5, 1, 0, 10, 20)
+
+        >>> print(pydt.mjd)
+        55317.00717592592
+        >>> pydt.mjd = 55489.01358
+
+        >>> print(pydt.ticks)
+        63423130773311999
+        >>> pydt.ticks = 63681941594000000
+
+        >>> observer_longitude = 6.67  # degrees
+        >>> print('GMST: {:9.6f} hours'.format(pydt.gmst()))
+        GMST:  4.959715 hours
+        >>> print('LMST: {:9.6f} hours'.format(pydt.lmst(observer_longitude)))
+        LMST:  5.076129 hours
+
+    One can also set the date and time manually with the
+    `~cysgp4.PyDateTime.set` method.
     '''
 
     # hold the C++ instance, which we're wrapping
@@ -117,6 +196,25 @@ cdef class PyDateTime(object):
 
     @classmethod
     def from_ticks(cls, unsigned long long ticks):
+        '''
+        Creates a new `~cysgp4.PyDateTime` instance from "ticks".
+
+        Internally, DateTime objects store time on a linear scale, "ticks",
+        which is the number of micro-seconds since 1. January 0001, 00:00.
+
+        Returns
+        -------
+        pydt : `~cysgp4.PyDateTime` object
+
+        Examples
+        --------
+        "from_ticks" is a classmethod::
+
+            >>> ticks = 58628880000000000  # aka MJD zero point
+            >>> pydt = PyDateTime.from_ticks(ticks)
+            >>> pydt
+            <PyDateTime: 1858-11-17 00:00:00.000000 UTC>
+        '''
 
         dt = cls(dt=None, init=False)
         dt.ticks = ticks
@@ -125,6 +223,26 @@ cdef class PyDateTime(object):
 
     @classmethod
     def from_mjd(cls, double mjd):
+        '''
+        Creates a new `~cysgp4.PyDateTime` instance from MJD.
+
+        `Modified Julian Date (MJD)
+        <https://en.wikipedia.org/wiki/Julian_day#Variants>`_, is a
+        timescale, which is often used in astronomy.
+
+        Returns
+        -------
+        pydt : `~cysgp4.PyDateTime` object
+
+        Examples
+        --------
+        "from_mjd" is a classmethod::
+
+            >>> mjd = 56458.123
+            >>> pydt = PyDateTime.from_mjd(mjd)
+            >>> pydt
+            <PyDateTime: 2013-06-15 02:57:07.199999 UTC>
+        '''
 
         dt = cls(dt=None, init=False)
         dt.mjd = mjd
@@ -159,7 +277,10 @@ cdef class PyDateTime(object):
             <int> dt.microsecond
             )
 
-    datetime = property(_get_datetime, _set_datetime, None)
+    datetime = property(
+        _get_datetime, _set_datetime, None,
+        doc='datetime (see Class documentation).'
+        )
 
     def set(
             self,
@@ -167,7 +288,24 @@ cdef class PyDateTime(object):
             int hour, int minute, int second, int microsecond
             ):
         '''
-        Initialize PyDateTime
+        Creates a new `~cysgp4.PyDateTime` instance from given date and time.
+
+        Parameters
+        ----------
+        year : int
+            Year of desired date.
+        month : int
+            Month of desired date.
+        day : int
+            Day of desired date.
+        hour : int
+            Hours of desired time.
+        minute : int
+            Minutes of desired time.
+        second : int
+            Seconds of desired time.
+        microsecond : int
+            Microseconds of desired time.
         '''
 
         self._cobj.Initialise(
@@ -189,7 +327,9 @@ cdef class PyDateTime(object):
         # AddTicks returns a new instance...
         self._cobj = self._cobj.AddTicks(ticks_new - ticks_old)
 
-    ticks = property(_get_ticks, _set_ticks, None)
+    ticks = property(
+        _get_ticks, _set_ticks, None, doc='Ticks (see Class documentation).'
+        )
 
     def _get_mjd(self):
         return mjd_from_ticks(self._cobj.Ticks())
@@ -205,7 +345,9 @@ cdef class PyDateTime(object):
         # AddTicks returns a new instance...
         self._cobj = self._cobj.AddTicks(ticks_new - ticks_old)
 
-    mjd = property(_get_mjd, _set_mjd, None)
+    mjd = property(
+        _get_mjd, _set_mjd, None, doc='MJD (see Class documentation).'
+        )
 
     def __str__(self):
 
@@ -216,17 +358,102 @@ cdef class PyDateTime(object):
         return '<PyDateTime: ' + self.__str__() + '>'
 
     def gmst(self):
+        '''
+        Greenwhich mean sidereal time (GMST) of current date/time.
 
+        Returns
+        -------
+        gmst : float
+            Greenwhich mean sidereal time (GMST) in (fractional) hours.
+        '''
         return self._cobj.ToGreenwichSiderealTime()
 
     def lmst(self, obslon_deg):
+        '''
+        Local mean sidereal time (LMST) of current date/time.
+
+        Parameters
+        ----------
+        obslon_deg : float
+            Geographic longitude of the observer.
+
+        Returns
+        -------
+        lmst : float
+            Local mean sidereal time (LMST) in (fractional) hours.
+        '''
 
         return self._cobj.ToLocalMeanSiderealTime(DEG2RAD * obslon_deg)
 
 
 cdef class PyTle(object):
     '''
-    Wrapper around (C++) Tle class
+    Thin wrapper around sgp4 (C++) Tle class.
+
+    `Two-line element sets
+    <https://en.wikipedia.org/wiki/Two-line_element_set>`_ contain the
+    parameters necessary to describe a satellite's orbit (around Earth).
+    Although the name suggests that two lines of strings are used,
+    in practice often three lines are defined, the first containing a
+    satellite name (and/or) ID. It is important to note that for many
+    satellites, the corresponding TLEs get outdated quickly. Especially for
+    low earth orbit satellites, one should query up-to-date information
+    at least once per day.
+
+    `~cysgp4.PyTle` is only used to parse the TLE strings and store the
+    orbital parameters for use by other sgp4 routines.
+
+    Parameters
+    ----------
+    name : str
+        Name/ID of satellite.
+    line_one : str
+        First TLE line string.
+    line_two : str
+        Second TLE line string.
+
+    Returns
+    -------
+    pydt : `~cysgp4.PyTle` object
+
+    Examples
+    --------
+    TLEs can be obtained from many sources, such as
+    `<https://celestrak.com/>`_::
+
+        >>> import requests
+        >>> from cysgp4 import PyTle
+
+        >>> url = 'http://celestrak.com/NORAD/elements/science.txt'
+        >>> ctrak_science = requests.get(url)
+        >>> all_lines = ctrak_science.text.split('\r\n')
+        >>> tle_list = list(zip(
+        ...     *tuple(all_lines[idx::3]
+        ...     for idx in range(3))
+        ...     ))
+        >>> len(tle_list)
+        >>> print(*tle_list[1], sep='\n')
+        HST
+        1 20580U 90037B   19321.38711875  .00000471  00000-0  17700-4 0  9991
+        2 20580  28.4699 288.8102 0002495 321.7771 171.5855 15.09299865423838
+
+        >>> hst_tle = PyTle(*tle_list[1])
+        >>> hst_tle
+        <PyTle: HST                     >
+        >>> print(hst_tle)
+        Norad Number:         20580
+        Int. Designator:      90037B
+        Epoch:                2019-11-17 09:17:27.060000 UTC
+        Orbit Number:         42383
+        Mean Motion Dt2:        0.00000471
+        Mean Motion Ddt6:       0.00000000
+        Eccentricity:           0.00024950
+        BStar:                  0.00001770
+        Inclination:           28.46990000
+        Right Ascending Node: 288.81020000
+        Argument Perigee:     321.77710000
+        Mean Anomaly:         171.58550000
+        Mean Motion:           15.09299865
     '''
 
     # hold the C++ instance, which we're wrapping
@@ -234,6 +461,12 @@ cdef class PyTle(object):
 
     def __init__(self, name, line_one, line_two):
 
+        # Note: as there is no empty constructor ("Tle()") in the sgp4
+        # library, we need to instantiate the object on the heap.
+        # This is because Cython can not (yet?) do something like
+        # my_tle = Tle(*args)
+        # without the empty constructor being available, even though it
+        # is not used.
         self.thisptr = new Tle(
             name.encode('UTF-8'),
             line_one.encode('UTF-8'),
