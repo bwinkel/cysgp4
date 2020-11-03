c05d56173d3b928c1ef81a6fdd619324b77c611d
Benjamin Winkel <bwinkel@mpifr.de>
2020-11-03 22:29:27 +0000

    Update docs for  ***NO_CI***

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,10 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.4.dev7+gc52ca9f',
+    VERSION: '0.3.4.dev11+gf6e8dc0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
